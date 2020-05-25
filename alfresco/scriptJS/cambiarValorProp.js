4211201876247ea78921812cc0df3e02c359cf7c
icrovett <icrovett@gmail.com>
2020-05-25 09:08:09 +0200

    cambiar propiedad
    Cambiar el valor de una propiedad de un documento que se ha seleccionado previamente.
    Navegar a un documento y seleccionar la opción de abrir en consola java script, de esta forma el objeto document ya está cargado con ese nodo de Alfresco.

---
+++ alfresco/scriptJS/cambiarValorProp.js
@@ -0,0 +1,7 @@
+//Meter valor a una propiedad concreta:
+document.properties["no:propiedad"] = false;
+//No olvidar salvar el cambio :)
+document.save();
+
+//Navegar a un documento y seleccionar la opción de abrir en consola java script, 
+//de esta forma el objeto document ya está cargado con ese nodo de Alfresco.
